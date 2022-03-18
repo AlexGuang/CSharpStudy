fa91fdc90368615d12e495ff9f3a8ddb5b003298
xiaoguangli <guangmvp@gmail.com>
2022-03-18 12:57:36 +1100

    this is a test 2

---
+++ TEst2/TEst2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TEst2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
