6bb60aec209ed9c3e7389c65ca8c9ba729c73196
xiaoguangli <guangmvp@gmail.com>
2022-03-19 13:14:20 +1100

    Knowledge and c# code of exceptions and some importan operators.

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int num2 = 3;
+            int num3;
+
+
+            // unary operators
+            num3 = -num1;
+            Console.WriteLine("num3 is: {0}", num3);
+
+            bool isSunny = true;
+            Console.WriteLine("is it sunny? \n {0}",!isSunny);
+
+
+            //increment operators
+            int num = 0;
+            num++;
+            Console.WriteLine("num is: {0}", num);
+            Console.WriteLine("num is: {0}", num++);//post increment
+            Console.WriteLine("num is: {0}", num);
+            // pre increment
+            Console.WriteLine("num is: {0}", ++num); //difference of num++ and ++num;
+            Console.WriteLine("num is: {0}", num);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Now we gonna do the decrement");
+            //decrement operator：num--
+            Console.WriteLine("num is: {0}", num--);//post decrement
+            Console.WriteLine("num is: {0}", num);
+            Console.WriteLine("num is: {0}", --num);// pre decrement
+            Console.WriteLine("num is: {0}", num);
+
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            int result;
+
+            result = num1 + num2;
+            Console.WriteLine("result of num1 + num2 is: {0}", result);
+            result = num1 - num2;
+            Console.WriteLine("result of num1 - num2 is: {0}", result);
+            result = num1 * num2;
+            Console.WriteLine("result of num1 * num2 is: {0}", result);
+            result = num1 / num2;
+            Console.WriteLine("result of num1 / num2 is: {0}", result);
+            result = num1 % num2;
+            Console.WriteLine("result of num1 % num2 is: {0}", result);
+
+            //relational and type operators
+            bool isLower;
+            isLower = num1 < num2;
+
+            Console.WriteLine("result of num1 < num2 is :{0}", isLower);
+
+            //equality operator
+            bool isEqual;
+            isEqual = num1 == num2;
+            Console.WriteLine("result of num1 == num2 is :{0}", isEqual);
+
+            isEqual = num1 != num2;
+            Console.WriteLine("result of num1 != num2 is :{0}", isEqual);
+
+
+            //conditional operator()
+            Console.WriteLine("isLower is :{0}", isLower);
+            bool isLowerAndSunny;
+            //conditon1 AND conditon2
+            isLowerAndSunny = isLower && isSunny;
+            Console.WriteLine("result of isLower && isSunny is: {0}", isLowerAndSunny);
+
+
+            //CONDITON1 OR CONDITION2
+            isLowerAndSunny = isLower || isSunny;
+            Console.WriteLine("result of isLower || isSunny is: {0}", isLowerAndSunny);
+
+
+        }
+    }
+}
---
+++ TryCatchFinally/TryCatchFinally/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace TryCatchFinally
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input the first number:");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Please input the second number:");
+            double b = double.Parse(Console.ReadLine());
+            double c =0;
+            c = a / b;
+            try
+            {
+                 c = a / b;
+            }
+            catch (Exception)//never happens, maybe the double type can divde zero, let me do an experiment of integer.
+
+            {
+
+                Console.WriteLine("1. C的值是：{0}", c); 
+
+            }
+            Console.WriteLine("C的值是：{0}",c);
+
+            int d = 5;
+            int e = 0;
+            //int result = d / e;//errror occurs,its DivideByZeroException'Attempted to divide by zero'. So we can
+            //write a try here
+            try
+            {
+                int result = d / e;
+            }
+            catch (DivideByZeroException)
+            {
+
+                Console.WriteLine("error, please do not divide zero, it cannot be divided by zero");
+            }
+
+
+            Console.WriteLine("Please enter a number");
+
+            string userInput = Console.ReadLine();
+            try
+            {
+                int userInputAsInt = int.Parse(userInput);
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Format exception, please enter the correct type next time");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Overflow exception, your input is too long or too short");
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Arguement exception, Please do not input empty or null content");
+            }
+            finally
+            {
+                Console.WriteLine("This is anyway"); //Anyway, it will execute.
+            }
+
+
+        }
+    }
+}
