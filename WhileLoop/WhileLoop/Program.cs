4e4089595427abe49c9719e51df05728acc200ae
xiaoguangli <guangmvp@gmail.com>
2022-03-20 13:32:13 +1100

    A very basic and simple while loop code project

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            while (counter < 10)
+            {
+                Console.WriteLine(counter);
+                counter++;
+            }
+        }
+    }
+}
