428d2a546fd6f955330b5424a2c8e4086e0762bf
xiaoguangli <guangmvp@gmail.com>
2022-03-20 12:16:09 +1100

    Very basic for loop code to print  all odd numbers from zero to twenty.

---
+++ ForLoop/ForLoop/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 10; counter++)
+            {
+                Console.WriteLine(counter);
+
+            }
+            Console.WriteLine("For loop is done!");
+
+
+            //Challenge 1:Use a for loop to print all odd numbers from zero to twenty.
+            Console.WriteLine("Please write a for loop to print odd numbers from zero to twenty");
+            for (int i = 1; i < 20; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("All odd numbers from zero to twenty have been printed.");
+        }
+    }
+}
