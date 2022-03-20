2ddbbedba82f63c0cb8d510258613c5e1bdd64c2
xiaoguangli <guangmvp@gmail.com>
2022-03-20 16:06:14 +1100

    Basic object oriented program. Learn how to create a class and the usage of property and method in a class.

---
+++ ObjectOrientedProgramLearning/ObjectOrientedProgramLearning/Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgramLearning
+{
+    //challenge:
+    // create a public  variable lastName of type string.
+    // change the IntroduceMyself method, so it tells the whole name.
+    // create two objects with full information about themself - firstName and lastName
+    internal class Human
+    {
+        //member variable
+        public string firstName;
+        public string lastName;
+
+        // member function
+        public void IntroduceMysel()
+        {
+            Console.WriteLine("Hi, I'm {0} {1} .",firstName,lastName);
+        }
+    }
+}
---
+++ ObjectOrientedProgramLearning/ObjectOrientedProgramLearning/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ObjectOrientedProgramLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           //create an object of my class
+           // an instance of Human
+            Human xiaoguang = new Human();
+            //access public variable from outside, and even change it
+            xiaoguang.firstName = "XIAOGUANG";
+            //call method of it
+            xiaoguang.IntroduceMysel();
+
+            Human jack = new Human();
+            jack.firstName = "JACK";
+            jack.lastName = "SMITH";
+
+
+            Human allen = new Human();
+            allen.firstName = "ALLEN";
+            allen.lastName = "SOON";
+
+            jack.IntroduceMysel();
+            allen.IntroduceMysel();
+
+
+
+        }
+    }
+}
