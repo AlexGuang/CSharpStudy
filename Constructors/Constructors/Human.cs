0f5be076a18da1d0c9df09656b4eac680365a0d5
xiaoguangli <guangmvp@gmail.com>
2022-03-21 11:30:44 +1100

    Simple example about build a constructor of a class. The usage of "this" should be paid more attention.

---
+++ Constructors/Constructors/Human.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    //Challenge:
+    // add two more member variable to "Human". eyeColor and age.
+    //adjust the constructor , so it requires all of the four values to be used, when creating an object of Human
+    //create two objects of type Human.
+    internal class Human
+    {
+        // member variable 
+       private string firstName;
+        private string lastName;
+        private string eyeColor;
+        private int age;
+
+        //constructor
+        public Human(string firstName, string lastName, string eyeColor,int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.eyeColor = eyeColor;
+            this.age = age;
+        }
+
+        public  Human(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        public Human(string myFirstName)
+        {
+            firstName = myFirstName;           
+        }
+        public Human()
+        {           
+
+        }
+
+        //member method
+        public void IntroduceMyself()
+        {
+            Console.WriteLine("Hi, I'm {0} {1}, my eye is {2}, and I am {3} years old",firstName,lastName,eyeColor,age);
+        }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Constructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Human olivia =new Human();
+            Human xiaoguang = new Human("xiaoguang");
+            Human alex = new Human("alex", "li");
+            olivia.IntroduceMyself();
+            xiaoguang.IntroduceMyself();
+            alex.IntroduceMyself();
+
+            Human jack = new Human("Jack", "Sun", "black", 18);
+           jack.IntroduceMyself();
+        }
+    }
+}
