eb74c5ada160128b2a37efd4925aea4a9f060ac4
xiaoguangli <guangmvp@gmail.com>
2022-03-21 20:37:22 +1100

    Practice property, geter and setter, constructor, class and instance. Moreover, finalizer.

---
+++ MembersFinalizerDestructor/MembersFinalizerDestructor/Members.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MembersFinalizerDestructor
+{
+    internal class Members
+    {
+        private string memberName;
+        private string jobTitle;
+
+        public int age;
+        private int salary;
+        public bool IsFriend { get; set; }
+        public int Salary
+        {
+
+            get
+            {
+                if (IsFriend)
+                {
+                    return salary;
+                }
+                else
+                    return 0;
+            }
+            set
+            {
+                salary = value;
+            } }
+        public string JobTitle
+        {
+            get { return jobTitle; }
+            set { jobTitle = value; }
+        }
+        public Members(string memberName, string jobTitle, int age)
+        {
+            this.memberName = memberName;
+            this.jobTitle = jobTitle;
+            this.age = age;
+
+            Console.WriteLine("Instance created");
+        }
+        public void IntroduceTheMember()
+        {
+            if (IsFriend)
+            {
+                ShowPrivateInfo();
+
+            }
+            else
+            {
+                Console.WriteLine("The member's name is {0},job is {1}, age is {2}",memberName,JobTitle,age);
+            }
+        }
+        private void ShowPrivateInfo()
+        {
+            Console.WriteLine("My,friend, my salary is: {0}",Salary);
+        }
+
+        ~Members()
+        {
+            Console.WriteLine("Instance finalized");
+        }
+    }
+}
---
+++ MembersFinalizerDestructor/MembersFinalizerDestructor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MembersFinalizerDestructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Members jack = new Members("Jack", "Manager", 32);
+            jack.Salary = 2000;
+            jack.IsFriend = false;
+            jack.IntroduceTheMember();
+
+            Members lucy = new Members("Lucy", "Custmer Manager", 22);
+            lucy.Salary = 5000;
+        
+            lucy.IntroduceTheMember();
+
+
+            Members alex = new Members("alex", "Tech Manager", 22);
+            alex.Salary = 100000;
+            alex.IsFriend = true;
+            alex.IntroduceTheMember();
+
+        }
+    }
+}
