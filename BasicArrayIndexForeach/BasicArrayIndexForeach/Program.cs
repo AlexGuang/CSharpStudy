3bec2f5abb6c7a0ef757613b9e3e9565d3ab67e7
xiaoguangli <guangmvp@gmail.com>
2022-03-21 22:56:03 +1100

    Some basic practice with foreach loop.

---
+++ BasicArrayIndexForeach/BasicArrayIndexForeach/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace BasicArrayIndexForeach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                nums[i] = i + 10;
+            }
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine("Element[{0}] = {1}", j, nums[j]);
+
+            }
+            Console.WriteLine(  "-------------------------");
+            int conter = 0;
+            foreach (int k in nums)
+            {
+                Console.WriteLine("Element[{0}] = {1}",conter,k);
+                conter++;
+            }
+            Console.WriteLine("===========================");
+            string[] bestFriends =
+            {
+                "Olivia",
+                "Jackey",
+                "Vennena",
+                "Hannah",
+                "Lily"
+            };
+            foreach (string k in bestFriends)
+            {
+                Console.WriteLine("Hi!{0}! How are you? ",k);
+            }
+        }
+    }
+}
