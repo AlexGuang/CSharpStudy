cccbb3d2dd556c3ef2c4ef63dd3119404ae31b8e
xiaoguangli <guangmvp@gmail.com>
2022-03-21 13:05:02 +1100

    In a setter, create a new Exception and throw it to make the program stronger.

---
+++ UseGeterAndSetterInClass/UseGeterAndSetterInClass/Box.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UseGeterAndSetterInClass
+{
+    internal class Box
+    {
+        private int length;
+        private int height;
+        private int width;
+        private int volume;
+
+
+        public Box(int length, int height, int width)
+        {
+            this.length = length;
+            this.height = height;
+            this.width = width;
+        }
+
+        public void SetLength(int length)
+        {
+            this.length=length;
+        }
+        private int GetVolume()
+        {
+            return this.length*this.height*this.width;
+        }
+
+        public void SetHight(int height)
+        {
+            if (height < 0)
+            {
+                throw new Exception("You should input a value which is greater than 0");
+                
+            }
+            else
+            {
+                this.height=height;
+            }
+        }
+
+        public void DescribeBox()
+        {
+            Console.WriteLine("The length,height,width and volume of this box are:{0},{1},{2},{3}",length,height,width,GetVolume());
+        }
+    }
+    
+}
---
+++ UseGeterAndSetterInClass/UseGeterAndSetterInClass/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UseGeterAndSetterInClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Box box = new Box(2, 3, 4);
+            box.DescribeBox();
+            box.SetLength(30);
+            box.DescribeBox();
+            
+
+        }
+    }
+}
