39311a7c5972afba5e02bf3023e8a1ddca1efc7b
xiaoguangli <guangmvp@gmail.com>
2022-03-22 15:06:07 +1100

    Basicly create a 2 dimentions array and display it with a for loop.

---
+++ Basic2DArrayAndNestedForLoop/Basic2DArrayAndNestedForLoop/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Basic2DArrayAndNestedForLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numArray =
+            {         
+                  { 1, 2, 3 }, 
+                  { 4, 5, 6 }, 
+                  { 7, 8, 9 },                
+            };
+
+            for (int i = 0; i < numArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < numArray.GetLength(1); j++)
+                {
+                    if (numArray[i,j] % 2 == 0)
+                    {
+                        Console.WriteLine(numArray[i, j]);
+                    }
+                }
+            }
+        }
+    }
+}
