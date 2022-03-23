d890f453143aacbbe50a1262989cfb6869bd655d
xiaoguangli <guangmvp@gmail.com>
2022-03-23 21:23:49 +1100

    Usage of "params" key word. Very useful

---
+++ ParamsKeyWords/ParamsKeyWords/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ParamsKeyWords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vs = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, -500 };
+            Console.WriteLine("The minimum value is :{0}", GetMinValue(vs));
+            int min = GetMinValue(4, 4, 2, 1, 0, -1, 100);
+            Console.WriteLine(min);
+        }
+        public static int GetMinValue(params int[] numbers)
+        {
+            int min = int.MaxValue;
+            foreach (int  item in numbers)
+            {
+                if (item < min)
+                {
+                    min = item;
+                }
+            }
+            return min;
+        }
+    }
+}
