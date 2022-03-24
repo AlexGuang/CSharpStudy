c277488689b04eb1b736f4e79dee005785d3a7cc
xiaoguangli <guangmvp@gmail.com>
2022-03-24 13:40:38 +1100

    Simple practice of List.

---
+++ ListPractice/ListPractice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+
+
+            for (int i = 100; i <= 170; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    numbers.Add(i);
+
+                }
+            }
+            for (int j = 0; j < numbers.Count; j++)
+            {
+                Console.Write(numbers[j] + " ");
+            }
+        }       
+
+    }
+}
