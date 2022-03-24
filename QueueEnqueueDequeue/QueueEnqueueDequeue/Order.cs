6d17b4b46f2f5c90910328f7b9dde1e2bc505042
xiaoguangli <guangmvp@gmail.com>
2022-03-24 21:48:49 +1100

    Use Queue solve a simple order processing sequence problem.

---
+++ QueueEnqueueDequeue/QueueEnqueueDequeue/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueEnqueueDequeue
+{
+    internal class Order
+    {
+        public int OrderId { get; set; }
+        public int  OrderQuantity { get; set; }
+        
+
+        public Order(int orderId, int orderQuantity)
+        {
+            this.OrderQuantity = orderQuantity;
+            this.OrderId = orderId;
+        }
+
+
+
+        public void ProcessOrder(int orderId)
+        {
+            Console.WriteLine($" Order No. {orderId} was  processed!");
+        }
+    }
+}
---
+++ QueueEnqueueDequeue/QueueEnqueueDequeue/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueEnqueueDequeue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<Order> queue = new Queue<Order>();
+
+            foreach ( Order o in ReceiveOrderFromBranchOne())
+            {
+                queue.Enqueue(o);
+            }
+
+            foreach (Order item in ReceiveOrderFromBranchTwo())
+            {
+                queue.Enqueue(item);
+            }
+
+            do
+            {
+                Order order = queue.Dequeue();
+                Console.WriteLine("Now the {0} order is processing",order.OrderId);
+                order.ProcessOrder(order.OrderId);
+
+
+            } while (queue.Count > 0);
+
+        }
+
+        public static Order[] ReceiveOrderFromBranchOne()
+        {
+            Order[] branch1 = new Order[]
+            {
+                new Order(1, 4),
+                new Order(2, 5),
+                new Order(6, 6),
+            };
+            return branch1;
+        }
+        public static Order[] ReceiveOrderFromBranchTwo()
+        {
+            Order[] branch2 = new Order[]
+            {
+                new Order(3, 10),
+                new Order(4, 15),
+                new Order(5, 16),
+            };
+            return branch2;
+        }
+    }
+}
