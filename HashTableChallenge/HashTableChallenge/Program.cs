fdbeea702c9c331257fbf0f2003a63934cb4aa44
xiaoguangli <guangmvp@gmail.com>
2022-03-24 16:18:46 +1100

    It's very fast to search a value in a hashtable if have a key

---
+++ HashTableChallenge/HashTableChallenge/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTableChallenge
+{
+    internal class Program
+    {
+        //Challenge:
+        /*Write a program that will iterate through each element of the students array and insert them into a hashtable.
+         * If a student with the same ID already exists in the hashtable skip it and display the following error:
+         * "Sorry, A student with the same ID already Exists".
+         * Hint: Use the method Containskey() to check wether a student with the same ID already exist.      
+         */
+        static void Main(string[] args)
+        {
+            Hashtable studentTable = new Hashtable();
+            int idInput = 0;
+            string idString;
+            string nameInput;
+            while (true)
+            {
+
+                    do
+                {
+                    Console.WriteLine("Please input the Id:");
+                    idString = Console.ReadLine();
+
+                } while (!int.TryParse(idString, out idInput));
+
+           
+                
+
+            
+                if (studentTable.ContainsKey(idInput))
+                {
+                    Console.WriteLine("Sorry, a student with the same ID already Exists");
+                }
+                else
+                {
+                    Console.WriteLine("Please input the Name:");
+                    nameInput = Console.ReadLine();
+                    studentTable.Add(idInput, nameInput);
+                    Console.WriteLine("Student with ID {0} was added.",idInput);
+                }
+            }
+          
+
+        }
+    }
+}
---
+++ HashTableChallenge/HashTableChallenge/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableChallenge
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public Student(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+    }
+}
