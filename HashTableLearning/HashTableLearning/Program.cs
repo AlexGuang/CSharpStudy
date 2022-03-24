00a0ef72e0b38ad878b5aa5dcea5a9b526cc5701
xiaoguangli <guangmvp@gmail.com>
2022-03-24 14:43:20 +1100

    learning of Hashtable  Remember Key to Value structure   DictionaryEntry.Value return an object

---
+++ HashTableLearning/HashTableLearning/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+namespace HashTableLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable studentTable = new Hashtable();
+
+            Student student1 = new Student( 1, "Marry", 84);
+            Student student2 = new Student(2, "Jackey", 93);
+            Student student3 = new Student(3, "Machiel", 83);
+
+            studentTable.Add(student1.id, student1);
+            studentTable.Add(student2.id, student2);
+            studentTable.Add(student3.id, student3);
+
+            Student storedStudent = (Student)studentTable[student1.id];
+            Console.WriteLine("id:{0}, name:{1}, GPA:{2}",storedStudent.id,storedStudent.name,storedStudent.GPA);
+            
+            Console.WriteLine("采用DictionaryEntry 作为元素变量类型");
+            foreach (DictionaryEntry entry in studentTable)
+            {
+                Student tem = (Student)entry.Value;// entry.Value return a object;   DictionaryEntry.Value return a object
+                Console.WriteLine("ID is:{0} ", tem.id);
+                Console.WriteLine("Name is: {0}", tem.name);
+                Console.WriteLine("GPA is:{0} ", tem.GPA);
+            }
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("采用class类名作为元素变量类型");
+            foreach (Student student in studentTable.Values)
+            {
+                Console.WriteLine("ID is:{0} ", student.id);
+                Console.WriteLine("Name is:{0} ", student.name);
+                Console.WriteLine("GPA is:{0} ", student.GPA);
+            }
+        }
+    }
+}
---
+++ HashTableLearning/HashTableLearning/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTableLearning
+{
+    public class Student
+    {
+        public int id;
+        public string name;
+        public int GPA;
+
+
+        public Student(int id, string name, int GPA)
+        {
+            this.id = id; 
+            this.name = name;
+            this. GPA = GPA;
+        }
+    }
+}
