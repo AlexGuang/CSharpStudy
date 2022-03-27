d6364b59245b82b8cb04563ee03adce15f64d9a1
xiaoguangli <guangmvp@gmail.com>
2022-03-27 21:41:50 +1100

    Inheriance learning, theory and some  practice.

---
+++ InheritanceLearning/InheritanceLearning/ElectronicDevice.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceLearning
+{
+    internal class ElectronicDevice
+    {
+        public bool IsOn { get; set; }
+    }
+}
