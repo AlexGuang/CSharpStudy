524c0dacc2b84102a62428187fa128b4ae4f98fc
xiaoguangli <guangmvp@gmail.com>
2022-03-29 17:17:06 +1100

    A simple example of Interface. A class inherited from an interface.

---
+++ InterfaceLearning/InterfaceLearning/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InterfaceLearning
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ticket ticket = new Ticket(29
+                );
+            Ticket ticket1 = new Ticket (29);
+            Console.WriteLine("{0}", ticket.Equals(ticket1));
+        }
+    }
+}
---
+++ InterfaceLearning/InterfaceLearning/Ticket.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceLearning
+{
+    internal class Ticket : IEquatable<Ticket>
+    {
+        public int Duration { get; set; }
+        public Ticket(int duration)
+        {
+            this.Duration = duration;
+        }
+
+        public bool Equals(Ticket otherticket)
+        {
+            return Duration == otherticket.Duration;
+        }
+    }
+}
