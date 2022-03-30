03def2fb4f23dbd5d651849b4d7a76df70ef0a41
xiaoguangli <guangmvp@gmail.com>
2022-03-30 22:19:13 +1100

    Read from a text file through system.io.file. readAllText() /ReadAllLine(url)

---
+++ ReadingFromATextFile/ReadingFromATextFile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ReadingFromATextFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Method one to read text from a file
+            string text = System.IO.File.ReadAllText(@"E:\Annnew Studio\C#study\Textfile.txt");
+            Console.WriteLine("The text in the file is\n {0}"   ,text);
+            Console.WriteLine("---------------------------------------------------------");
+            // Method two
+
+            string[] lines = System.IO.File.ReadAllLines(@"E:\Annnew Studio\C#study\Textfile.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine("\t{0}",line);
+            }
+
+
+        }
+    }
+}
