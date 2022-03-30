85023356eace7d7b132f68c23d6c62b607e8258a
xiaoguangli <guangmvp@gmail.com>
2022-03-30 13:49:29 +1100

    Sealed Keywords to ban the override method from the base classes.

--- ChallengeOfInheritance/ChallengeOfInheritance/BMW.cs
+++ ChallengeOfInheritance/ChallengeOfInheritance/BMW.cs
@@ -12,6 +12,7 @@ namespace ChallengeOfInheritance
         public string Model { get; set; }
         private string brand = "BMW";
 
+
         public BMW(int hp, string color, string model) : base(hp, color)
         {
             SetYear = 1929;
@@ -23,7 +24,7 @@ namespace ChallengeOfInheritance
         {
             Console.WriteLine("This {3} car is {0},which is set up in {1}, the model is {2}, and it has {4} horse power!",brand,SetYear,Model,Color,HP);
         }
-        public override void Repair()
+        public sealed override void Repair()
         {
             Console.WriteLine("The {0}  {1} is broken!",Color,brand);
             base.Repair();
---
+++ ChallengeOfInheritance/ChallengeOfInheritance/M3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeOfInheritance
+{
+    internal class M3:BMW
+    {
+        public M3(int hp, string color, string model) : base(hp, color, model)
+        {
+            SetYear = 1929;
+            Model = model;
+
+        }
+      /*  public override void Repair()//Class BMW has sealed the method Repair(), so this child class cannot override it.
+        {
+            base.Repair();
+        }
+      */
+    }
+}
