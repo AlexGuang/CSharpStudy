ecc19c58f927c3a4e13e014a1097c26fd9392e0a
xiaoguangli <guangmvp@gmail.com>
2022-03-30 11:55:18 +1100

    Put IEnumerable<>as parameter and transfer to a Method.

---
+++ IEnumerableAsParameter/IEnumerableAsParameter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IEnumerableAsParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,4,5,6,7,8 };
+
+
+            int[] array = new int[] { 1, 2, 3,4, 5, 6, 7, 8 };
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+
+            queue.Enqueue(3);
+            queue.Enqueue(4);
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+
+            Console.WriteLine("Sum of list is {0}",GetSumOfIEnumerable(list));
+            Console.WriteLine("Sum of array is {0}", GetSumOfIEnumerable(array));
+            Console.WriteLine("Sum of queue is {0}",GetSumOfIEnumerable(queue));
+
+
+        }
+        public static int GetSumOfIEnumerable(IEnumerable<int> collection)
+        {
+            int sum = 0;
+            foreach (int number in collection)
+            {
+                sum += number;
+            }
+            return sum;
+        }
+    }
+}
