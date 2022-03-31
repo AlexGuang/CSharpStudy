40a755c9a46d55f3501936f7e956b740733fae60
xiaoguangli <guangmvp@gmail.com>
2022-03-31 12:21:07 +1100

    Usage of Enum

---
+++ ENUMLearning/ENUMLearning/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ENUMLearning
+{
+    internal class Program
+    {
+        enum Month
+        {
+            Januray,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            Decmber
+
+        }
+        static void Main(string[] args)
+        {
+            Month month1 = Month.Januray;
+            Console.WriteLine("{0}",month1);
+            Month month2 = Month.February;
+            Month month3 = Month.February;
+            Console.WriteLine(month2 == month3);
+        }
+    }
+}
