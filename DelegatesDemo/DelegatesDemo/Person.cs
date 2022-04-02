0a16391e6465b8cdd66f19f64acceacda9fcb204
xiaoguangli <guangmvp@gmail.com>
2022-04-02 21:51:15 +1100

    A delegate demo to replace a method inside a class

---
+++ DelegatesDemo/DelegatesDemo/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesDemo
+{
+    internal class Person
+    {
+        //name property
+        public string Name { get; set; }
+        //age property
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
---
+++ DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DelegatesDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>()
+           {
+               new Person("Jack",19),
+               new Person("Marry",20),
+               new Person("Danis",22)
+           };
+
+            DisplayPeople("adult", people, Ifadult);
+        }
+
+
+        public static bool Ifadult(Person person)
+        {
+            if (person.Age < 21)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        public delegate bool FilterDelegate(Person person);
+        public static void DisplayPeople( string title, List<Person> people,FilterDelegate filter)
+        {
+            foreach (var person in people)
+            {
+                if (filter(person))
+                {
+                    Console.WriteLine("The {0} is {1} years old, an {2}.",person.Name,person.Age,title);
+                }
+               
+            }
+        }
+
+    }
+   
+}
