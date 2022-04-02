12f3cb657ad7257dbf504981b3ba83802fce9dac
xiaoguangli <guangmvp@gmail.com>
2022-04-02 23:40:58 +1100

    A simple demo of delegate and events

---
+++ DelegateAndEvents/DelegateAndEvents/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegateAndEvents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
