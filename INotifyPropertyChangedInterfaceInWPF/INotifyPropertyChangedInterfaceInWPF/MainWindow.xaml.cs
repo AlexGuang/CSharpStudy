b649a9701f72cc5cd3cbb58d0e5339dcd6af88a7
xiaoguangli <guangmvp@gmail.com>
2022-04-05 17:38:03 +1000

    INotifyProperty Changed interface in WPF

---
+++ INotifyPropertyChangedInterfaceInWPF/INotifyPropertyChangedInterfaceInWPF/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace INotifyPropertyChangedInterfaceInWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Sum SumObj { get; set; }  
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SumObj = new Sum { Num1 = "1", Num2 = "3" };
+           
+
+            this.DataContext = SumObj;
+        }
+    }
+}
---
+++ INotifyPropertyChangedInterfaceInWPF/INotifyPropertyChangedInterfaceInWPF/Sum.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel;
+
+namespace INotifyPropertyChangedInterfaceInWPF
+{
+    public class Sum :INotifyPropertyChanged
+    {
+        private string num1;
+        private string num2;
+        private string result;
+        public string Num1 
+        { 
+            get {
+
+                     return num1;
+                }
+            set {
+                int num;
+                bool res = int.TryParse(value, out num);
+                if (res)
+                {
+                    num1 = value;
+                }
+                    OnPropertyChanged("Num1");
+                    OnPropertyChanged("Result");
+                
+
+            }
+        }
+        public string Num2
+        {
+            get
+            {
+
+                return num2;
+            }
+            set
+            {
+                int num;
+                bool res = int.TryParse(value, out num);
+                if (res)
+                {
+                    num2 = value;
+                   
+                }
+                OnPropertyChanged("Num2");
+                OnPropertyChanged("Result");
+            }
+        }
+        public string Result 
+        { 
+            get
+            {
+                int res = int.Parse(Num1) + int.Parse(Num2);
+                return res.ToString();
+            }
+
+            set 
+            {
+                int res = int.Parse(Num1) + int.Parse(Num2);
+                result = res.ToString();
+                OnPropertyChanged("Result");
+
+
+
+            } 
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this,new PropertyChangedEventArgs(property));
+
+            }
+        }
+    }
+}
