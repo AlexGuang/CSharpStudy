a7e82f474d79c463791a2b68126bc323717a9f19
xiaoguangli <guangmvp@gmail.com>
2022-04-05 12:27:48 +1000

    Dependence Property

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int MyProperty
+        {
+            get { return (int)GetValue(myDependencyProperty); }
+            set {  SetValue(myDependencyProperty,value); }
+        }
+
+        public static readonly DependencyProperty myDependencyProperty =
+            DependencyProperty.Register("MyProperty",typeof(int),typeof(MainWindow), new PropertyMetadata(0));
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
