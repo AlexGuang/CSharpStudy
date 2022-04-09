93c13b7ed07d4270cd68874d679a64fa88c69c86
xiaoguangli <guangmvp@gmail.com>
2022-04-09 11:27:34 +1000

    A simple demo using linq query

---
+++ LinqSimpleDemo/LinqSimpleDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace LinqSimpleDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+            OddNumbers(numbers);
+        }
+
+        static void OddNumbers(int[] numbers)
+        {
+            Console.WriteLine("Odd Number:");
+            IEnumerable<int> oddNumbers = from number in numbers where number % 2 == 1 select number;
+            Console.WriteLine(oddNumbers);
+            foreach (int oddNumber in oddNumbers)
+            {
+                Console.WriteLine(oddNumber);
+            }
+        }
+    }
+}
