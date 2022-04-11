05313c798423d30e84f4be8e587163e911bc795e
xiaoguangli <guangmvp@gmail.com>
2022-04-11 12:12:46 +1000

    Use lambda expression to select an object from the database and then insert new objects in a new table related to the first object int the foremal table

--- LinqToSQL/LinqToSQL/LinqToSQLDaterSeter.designer.cs
+++ LinqToSQL/LinqToSQL/LinqToSQLDaterSeter.designer.cs
@@ -33,6 +33,9 @@ namespace LinqToSQL
     partial void InsertUniversity(University instance);
     partial void UpdateUniversity(University instance);
     partial void DeleteUniversity(University instance);
+    partial void InsertStudent(Student instance);
+    partial void UpdateStudent(Student instance);
+    partial void DeleteStudent(Student instance);
     #endregion
 		
 		public LinqToSQLDaterSeterDataContext() : 
@@ -72,6 +75,14 @@ namespace LinqToSQL
 				return this.GetTable<University>();
 			}
 		}
+		
+		public System.Data.Linq.Table<Student> Students
+		{
+			get
+			{
+				return this.GetTable<Student>();
+			}
+		}
 	}
 	
 	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.University")]
@@ -89,6 +100,8 @@ namespace LinqToSQL
 		private string _Country;
 		
 		private int _RankedNum;
+		
+		private EntitySet<Student> _Students;
 		
     #region Extensibility Method Definitions
     partial void OnLoaded();
@@ -108,10 +121,11 @@ namespace LinqToSQL
 		
 		public University()
 		{
+			this._Students = new EntitySet<Student>(new Action<Student>(this.attach_Students), new Action<Student>(this.detach_Students));
 			OnCreated();
 		}
 		
-		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", DbType="Int NOT NULL", IsPrimaryKey=true, IsDbGenerated=true, UpdateCheck=UpdateCheck.WhenChanged)]
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL", IsPrimaryKey=true, IsDbGenerated=true, UpdateCheck=UpdateCheck.WhenChanged)]
 		public int Id
 		{
 			get
@@ -207,6 +221,230 @@ namespace LinqToSQL
 					this._RankedNum = value;
 					this.SendPropertyChanged("RankedNum");
 					this.OnRankedNumChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="University_Student", Storage="_Students", ThisKey="Id", OtherKey="UniversityId")]
+		public EntitySet<Student> Students
+		{
+			get
+			{
+				return this._Students;
+			}
+			set
+			{
+				this._Students.Assign(value);
+			}
+		}
+		
+		public event PropertyChangingEventHandler PropertyChanging;
+		
+		public event PropertyChangedEventHandler PropertyChanged;
+		
+		protected virtual void SendPropertyChanging()
+		{
+			if ((this.PropertyChanging != null))
+			{
+				this.PropertyChanging(this, emptyChangingEventArgs);
+			}
+		}
+		
+		protected virtual void SendPropertyChanged(String propertyName)
+		{
+			if ((this.PropertyChanged != null))
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+		
+		private void attach_Students(Student entity)
+		{
+			this.SendPropertyChanging();
+			entity.University = this;
+		}
+		
+		private void detach_Students(Student entity)
+		{
+			this.SendPropertyChanging();
+			entity.University = null;
+		}
+	}
+	
+	[global::System.Data.Linq.Mapping.TableAttribute(Name="dbo.Student")]
+	public partial class Student : INotifyPropertyChanging, INotifyPropertyChanged
+	{
+		
+		private static PropertyChangingEventArgs emptyChangingEventArgs = new PropertyChangingEventArgs(String.Empty);
+		
+		private int _Id;
+		
+		private string _Name;
+		
+		private int _Age;
+		
+		private string _Gender;
+		
+		private int _UniversityId;
+		
+		private EntityRef<University> _University;
+		
+    #region Extensibility Method Definitions
+    partial void OnLoaded();
+    partial void OnValidate(System.Data.Linq.ChangeAction action);
+    partial void OnCreated();
+    partial void OnIdChanging(int value);
+    partial void OnIdChanged();
+    partial void OnNameChanging(string value);
+    partial void OnNameChanged();
+    partial void OnAgeChanging(int value);
+    partial void OnAgeChanged();
+    partial void OnGenderChanging(string value);
+    partial void OnGenderChanged();
+    partial void OnUniversityIdChanging(int value);
+    partial void OnUniversityIdChanged();
+    #endregion
+		
+		public Student()
+		{
+			this._University = default(EntityRef<University>);
+			OnCreated();
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Id", AutoSync=AutoSync.OnInsert, DbType="Int NOT NULL IDENTITY", IsPrimaryKey=true, IsDbGenerated=true)]
+		public int Id
+		{
+			get
+			{
+				return this._Id;
+			}
+			set
+			{
+				if ((this._Id != value))
+				{
+					this.OnIdChanging(value);
+					this.SendPropertyChanging();
+					this._Id = value;
+					this.SendPropertyChanged("Id");
+					this.OnIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Name", DbType="NVarChar(50) NOT NULL", CanBeNull=false)]
+		public string Name
+		{
+			get
+			{
+				return this._Name;
+			}
+			set
+			{
+				if ((this._Name != value))
+				{
+					this.OnNameChanging(value);
+					this.SendPropertyChanging();
+					this._Name = value;
+					this.SendPropertyChanged("Name");
+					this.OnNameChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Age", DbType="Int NOT NULL")]
+		public int Age
+		{
+			get
+			{
+				return this._Age;
+			}
+			set
+			{
+				if ((this._Age != value))
+				{
+					this.OnAgeChanging(value);
+					this.SendPropertyChanging();
+					this._Age = value;
+					this.SendPropertyChanged("Age");
+					this.OnAgeChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_Gender", DbType="NVarChar(50) NOT NULL", CanBeNull=false)]
+		public string Gender
+		{
+			get
+			{
+				return this._Gender;
+			}
+			set
+			{
+				if ((this._Gender != value))
+				{
+					this.OnGenderChanging(value);
+					this.SendPropertyChanging();
+					this._Gender = value;
+					this.SendPropertyChanged("Gender");
+					this.OnGenderChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.ColumnAttribute(Storage="_UniversityId", DbType="Int NOT NULL")]
+		public int UniversityId
+		{
+			get
+			{
+				return this._UniversityId;
+			}
+			set
+			{
+				if ((this._UniversityId != value))
+				{
+					if (this._University.HasLoadedOrAssignedValue)
+					{
+						throw new System.Data.Linq.ForeignKeyReferenceAlreadyHasValueException();
+					}
+					this.OnUniversityIdChanging(value);
+					this.SendPropertyChanging();
+					this._UniversityId = value;
+					this.SendPropertyChanged("UniversityId");
+					this.OnUniversityIdChanged();
+				}
+			}
+		}
+		
+		[global::System.Data.Linq.Mapping.AssociationAttribute(Name="University_Student", Storage="_University", ThisKey="UniversityId", OtherKey="Id", IsForeignKey=true, DeleteOnNull=true, DeleteRule="CASCADE")]
+		public University University
+		{
+			get
+			{
+				return this._University.Entity;
+			}
+			set
+			{
+				University previousValue = this._University.Entity;
+				if (((previousValue != value) 
+							|| (this._University.HasLoadedOrAssignedValue == false)))
+				{
+					this.SendPropertyChanging();
+					if ((previousValue != null))
+					{
+						this._University.Entity = null;
+						previousValue.Students.Remove(this);
+					}
+					this._University.Entity = value;
+					if ((value != null))
+					{
+						value.Students.Add(this);
+						this._UniversityId = value.Id;
+					}
+					else
+					{
+						this._UniversityId = default(int);
+					}
+					this.SendPropertyChanged("University");
 				}
 			}
 		}
--- LinqToSQL/LinqToSQL/MainWindow.xaml.cs
+++ LinqToSQL/LinqToSQL/MainWindow.xaml.cs
@@ -29,27 +29,28 @@ namespace LinqToSQL
 
             string connectString = ConfigurationManager.ConnectionStrings["LinqToSQL.Properties.Settings.XiaoguangDBConnectionString"].ConnectionString;
             linqToSQLDaterSeterDataContext = new LinqToSQLDaterSeterDataContext(connectString);
-            InsertUniversities();
-         //  InsertStudents();
+         //   InsertUniversities();
+           InsertStudents();
 
         }
-        public void InsertUniversities()
+
+        public void InsertYaleUniversitis()
         {
-             //linqToSQLDaterSeterDataContext.ExecuteCommand("delete from University");
+           // linqToSQLDaterSeterDataContext.ExecuteCommand("delete from University");
             //linqToSQLDaterSeterDataContext.ExecuteCommand("SET IDENTITY_INSERT University ON");
             University yale = new University();
-            
+
             yale.Name = "Yale";
-           yale.RankedNum = 20;
+            yale.RankedNum = 20;
             yale.Country = "USA";
             yale.Location = "Decosasos";
 
             linqToSQLDaterSeterDataContext.Universities.InsertOnSubmit(yale);
             linqToSQLDaterSeterDataContext.SubmitChanges();
-           // linqToSQLDaterSeterDataContext.ExecuteCommand("SET IDENTITY_INSERT University OFF");
-            MainDataGrid.ItemsSource = linqToSQLDaterSeterDataContext.Universities;
+            linqToSQLDaterSeterDataContext.ExecuteCommand("SET IDENTITY_INSERT University OFF");
+           // MainDataGrid.ItemsSource = linqToSQLDaterSeterDataContext.Universities;
         }
-        //public void InsertStudents()
+        //public void InsertStudents2()
         //{
         //    Student xiaoming = new Student();
         //    xiaoming.Name = "小明";
@@ -62,5 +63,58 @@ namespace LinqToSQL
 
         //    MainDataGrid.ItemsSource = linqToSQLDaterSeterDataContext.Students;
         //}
+        /// <summary>
+        /// Insert an object into the database
+        /// </summary>
+        public void InsertUniversities()
+        {
+            University university = new University();// Create a new object
+
+            university.Name = "Tasmania University";
+            university.Country = "Australia";
+            university.RankedNum = 186;
+            university.Location = "Tasmania";   // Initialize the object
+
+
+            University university1 = new University();
+            university1.Name = "Beijing Tech";
+            university1.RankedNum = 24;
+            university1.Country = "China";
+            university1.Location = "Beijing";
+
+
+            linqToSQLDaterSeterDataContext.Universities.InsertOnSubmit(university);
+            linqToSQLDaterSeterDataContext.Universities.InsertOnSubmit(university1);// Submit the object to datacontext
+
+            linqToSQLDaterSeterDataContext.SubmitChanges(); // Submit the datacontext to database
+
+            MainDataGrid.ItemsSource = linqToSQLDaterSeterDataContext.Universities; // Show the database in app
+
+        }
+
+        public void InsertStudents()
+        {
+            University yale = linqToSQLDaterSeterDataContext.Universities.First(( un => un.Name == "Yale"));
+            University beijingTech = linqToSQLDaterSeterDataContext.Universities.First(un => un.Name == "Beijing Tech");
+            University tasmania = linqToSQLDaterSeterDataContext.Universities.First( un => un.Name == "Tasmania University");
+
+            List<Student> students = new List<Student>();
+            students.Add(new Student { Name = "Candy", Age = 19, Gender = "female", University = beijingTech });
+            students.Add(new Student { Name = "Frank", Age = 22, Gender = "male", UniversityId = 4 });
+            students.Add(new Student { Name = "Joe", Age = 18, Gender = "male", University = beijingTech });
+            students.Add(new Student { Name = "Micheal", Age = 17, Gender = "male", UniversityId = 9 });
+            students.Add(new Student { Name = "Marry", Age = 20, Gender = "female", UniversityId = 5 });
+            students.Add(new Student { Name = "Lydia", Age = 21, Gender = "female", University = tasmania });
+            students.Add(new Student { Name = "Jeams", Age = 19, Gender = "male", UniversityId = 9 });
+            students.Add(new Student { Name = "Mick", Age = 22, Gender = "male", UniversityId = 6 });
+            students.Add(new Student { Name = "Lorua", Age = 17, Gender = "female", University = beijingTech});
+
+
+
+            linqToSQLDaterSeterDataContext.Students.InsertAllOnSubmit(students); // Insert objects to the data context
+            linqToSQLDaterSeterDataContext.SubmitChanges();// Upload the datacontext to database
+
+            MainDataGrid.ItemsSource = linqToSQLDaterSeterDataContext.Students;// Show the updated database on the application
+        }
     }
 }
