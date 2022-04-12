4ac7477c853c0a36f8dac25789a49d468dbe75b8
xiaoguangli <guangmvp@gmail.com>
2022-04-12 12:19:45 +1000

    Thread starts, ends and completion Some property and methods to use

---
+++ ThreadStartAndEndCompletion/ThreadStartAndEndCompletion/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadStartAndEndCompletion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var taskCompletionSource = new TaskCompletionSource<bool>();
+
+            Thread thread = new Thread(() =>
+            {
+                Console.WriteLine($"Thread:{Thread.CurrentThread.ManagedThreadId}  starts");
+                Thread.Sleep(5000);
+                taskCompletionSource.TrySetResult(true);
+                Console.WriteLine($"Thread:{Thread.CurrentThread.ManagedThreadId} ends");
+
+            });
+            thread.Start();
+            var text = taskCompletionSource.Task.Result;
+            Console.WriteLine("task was done: {0}",text);
+        }
+    }
+}
