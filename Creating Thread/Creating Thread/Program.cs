72d127115981b6ac960f380789192b0580eaa7d5
xiaoguangli <guangmvp@gmail.com>
2022-04-12 11:51:42 +1000

    Creat many threads at the same time

---
+++ Creating Thread/Creating Thread/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Creating_Thread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            new Thread(() => {
+
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 1");
+                
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 2");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 3");
+            }).Start();
+
+            new Thread(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Thread 4");
+            }).Start();
+            //The four threads begin at same time,so order of print can be random.
+        }
+    }
+}
