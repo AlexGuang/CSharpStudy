d9f7bac8595ac37e19d856f14d283c54fc353e2d
xiaoguangli <guangmvp@gmail.com>
2022-04-12 12:44:23 +1000

    Threadpool and thread in backgroud

---
+++ ThreadPoolThreadInBackgroud/ThreadPoolThreadInBackgroud/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+
+namespace ThreadPoolThreadInBackgroud
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            new Thread(() =>
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starts.");
+                Thread.Sleep(1000);
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} ends.");
+            })
+            { IsBackground = true }.Start();
+            Enumerable.Range(0, 1000).ToList().ForEach(f =>
+
+            {
+                 ThreadPool.QueueUserWorkItem((o) =>
+                 {
+                     Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} starts.");
+                     Thread.Sleep(1000);
+                     Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} ends.");
+                 }) ;
+  
+            });
+        }
+    }
+}
