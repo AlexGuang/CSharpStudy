5b533a90158f0f62c0591d196b0db22b57c7b14f
xiaoguangli <guangmvp@gmail.com>
2022-04-22 12:36:28 +1000

    Generic Type as  papramiter, return and local variable

---
+++ GenericTypeMethod/GenericTypeMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GenericTypeMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Show(123);
+            Show("haha");
+            Show(3232.245);
+            Show1<int>();
+
+
+
+        }
+
+        public static void Show<T>(T param)//当参数使用
+        {
+            Console.WriteLine(param.GetType().Name);
+        }
+
+        public static T Show1<T>()//当返回值使用
+        {
+            return default(T);
+        }
+        public static void show2<T>()//当局部变量使用
+        {
+            T t;
+        }
+    }
+}
